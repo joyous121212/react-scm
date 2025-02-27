a544fa51810b5911df82c1d0c33a6e3120cd4520
joyous121212 <joyous95@gmail.com>
2025-02-27 17:05:08 +0900

    noticeList, noticeDetail

--- src/components/page/Management/Notice/NoticeMain/NoticeMain.tsx
+++ src/components/page/Management/Notice/NoticeMain/NoticeMain.tsx
@@ -1,9 +1,59 @@
+import { useLocation } from "react-router-dom";
 import { StyledTable, StyledTd, StyledTh } from "../../../../common/styled/StyledTable";
+import axios, { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
+import { PageNavigate } from "../../../../common/pageNavigation/PageNavigate";
+import { NoticeModal } from "../NoticeModal/NoticeModal";
+import { Portal } from "../../../../common/potal/Portal";
+import { modalState } from "../../../../../stores/modalState";
+import { useRecoilState } from "recoil";
+
+export interface INotice {
+    noticeId: number,
+    title: string,
+    content: string,
+    author: string,
+    createdDate: string,
+}
+
+interface INoticeListBodyResponse {
+    noticeList: INotice[],
+    noticeCnt: number
+}
 
 export const NoticeMain = () => {
+    const { search } = useLocation();
+    const [noticeList, setNoticeList] = useState<INotice[]>([]);
+    const [noticeCount, setNoticeCount] = useState<number>(0);
+    const [cPage, setCPage] = useState<number>(0);
+    const [modal, setModal] = useRecoilState<boolean>(modalState);
+    const [noticeId, setNoticeId] = useState<number>(0);
+
+    useEffect(() => {
+        searchNoticeList();
+    }, [search]);
+
+    const searchNoticeList = (currentPage?: number) => {
+        currentPage = currentPage || 1;
+        const searchParam = new URLSearchParams(search); // URLSearchParams - key, value 자동으로 나눠줌
+        searchParam.append("currentPage", currentPage.toString());
+        searchParam.append("pageSize", "5");
+
+        axios.post("/management/noticeListBody.do", searchParam).then((res: AxiosResponse<INoticeListBodyResponse>) => {
+            setNoticeList(res.data.noticeList);
+            setNoticeCount(res.data.noticeCnt);
+            setCPage(currentPage);
+        });
+    }
+
+    const handlerModal = (id: number) => {
+        setModal(!modal);
+        setNoticeId(id);
+    }
+
     return (
         <>
-            총 갯수 : 현재 페이지 : 0
+            총 갯수 {noticeCount} : 현재 페이지 : {cPage}
             <StyledTable>
                 <thead>
                     <tr>
@@ -14,11 +64,39 @@ export const NoticeMain = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <StyledTd colSpan={3}>데이터가 없습니다.</StyledTd>
-                    </tr>
+                    {
+                        noticeList?.length > 0 ? (
+                            noticeList.map((notice) => {
+                                return (
+                                    <tr key={notice.noticeId}>
+                                        <StyledTd>{notice.noticeId}</StyledTd>
+                                        <StyledTd onClick={() => handlerModal(notice.noticeId)}>{notice.title}</StyledTd>
+                                        <StyledTd>{notice.author}</StyledTd>
+                                        <StyledTd>{notice.createdDate}</StyledTd>
+                                    </tr>
+                                )
+                            })
+                        ) : (
+                            <tr>
+                                <StyledTd colSpan={4}>데이터가 없습니다.</StyledTd>
+                            </tr>
+                        )
+                    }
+                    
                 </tbody>
             </StyledTable>
+            <PageNavigate 
+                totalItemsCount={noticeCount} 
+                onChange={searchNoticeList} 
+                itemsCountPerPage={5} 
+                activePage={cPage}
+            />
+            {modal && (
+                <Portal>
+                    <NoticeModal noticeId={noticeId} setNoticeId={setNoticeId} />
+                </Portal>
+            )}
+            
         </>
     );
 };
