11bc629de222192fdfdb3f4bc30d64798f83bd6c
joyous121212 <joyous95@gmail.com>
2025-03-07 09:09:21 +0900

    modal table css

--- src/components/page/Mall/Products/ProductsModal/styled.ts
+++ src/components/page/Mall/Products/ProductsModal/styled.ts
@@ -16,7 +16,7 @@ export const ProductsModalStyled = styled.div`
         padding: 20px;
         border-radius: 8px;
         box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
-        width: 500px;
+        width: auto;
     }
 
     table {
